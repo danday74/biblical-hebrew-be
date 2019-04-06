62e19e8ea59efb9ebdee715b3b8b16c574ad6ac9
Daniel Lewis <daniellewis777@gmail.com>
2019-04-06 01:54:01 +0100

    added support for multiple origins

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const guestUserName = 'Guest'
 const guestUserSlug = slugify(guestUserName, {lower: true})
 
 const config = {
-  corsOptions: {origin: 'http://localhost:4000'},
+  corsOptions: {origin: ['http://localhost:4000', 'http://localhost:4001']},
   defaultUsers: {
     [guestUserSlug]: {
       slug: guestUserSlug,
