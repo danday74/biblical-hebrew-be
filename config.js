9c84066fbf0fa5f6cab3ef23c60660355f2f3448
Daniel Lewis <daniellewis777@gmail.com>
2019-04-08 19:45:45 +0100

    added new definition of a user

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
   defaultUsers: {
     [guestUserSlug]: {
       slug: guestUserSlug,
-      name: guestUserName,
+      username: guestUserName,
       password: 'test'
     }
   },
