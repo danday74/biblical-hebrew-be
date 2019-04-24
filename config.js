a0705db4d0224f51169f600f0f9934cd1c1c8e76
Daniel Lewis <daniellewis777@gmail.com>
2019-04-24 03:20:59 +0100

    passwords must be 6 chars

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-const guestUserName = 'Guest'
+const guestUserName = 'Guest888'
 
 const config = {
   defaultUsers: {
     [guestUserName]: {
       username: guestUserName,
-      password: 'test'
+      password: 'Yeshua'
     }
   },
   port: 4002,
   recaptcha: {
     secret: 'NkxlZXdKMFVBQUFBQUR1Y0xDcVMzVnNFOEx5YUg2V2QxNTBPNE5ORw=='
   },
-  usernameAndPasswordRegex: /^[a-zA-Z0-9\u0590-\u05FF]{3,15}$/,
+  usernameAndPasswordRegex: /^[a-zA-Z0-9\u0590-\u05FF]{6,15}$/,
   usernameAndPasswordRegexNoLength: /^[a-zA-Z0-9\u0590-\u05FF]*$/
 }
 
